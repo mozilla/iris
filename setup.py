f229cec88998f8c91aab58f4e8f8a6ce594d0f53
Matt Wobensmith <mwobensmith@mozilla.com>
2019-12-04 09:24:43 -0800

    Bump version to 0.8.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import platform
 from setuptools import setup, find_packages
 
 PACKAGE_NAME = "moziris"
-PACKAGE_VERSION = "0.8.2"
+PACKAGE_VERSION = "0.8.3"
 
 INSTALL_REQUIRES = [
     "bugzilla==1.0.0",
